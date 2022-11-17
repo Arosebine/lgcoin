a5fa63ac58e560dfb765b967445961d17c1fef9c
arosebine <arebine@gmail.com>
2022-11-17 21:21:46 +0100

    worked on controller, middleware, model, utils

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
 
 
 const indexRouter = require('./routes/index');
@@ -25,6 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
