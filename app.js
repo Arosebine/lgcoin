72e2b8eb1650f42995e227bf989393d14940696a
arosebine <arebine@gmail.com>
2022-11-22 09:57:02 +0100

    modified the engine template

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ connectDB();
 
 const port = process.env.PORT || 7630;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
