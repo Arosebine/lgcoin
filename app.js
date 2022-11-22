e90e6c3ac879c529a1aa631dbcd5f38a63521b19
arosebine <arebine@gmail.com>
2022-11-22 20:35:34 +0100

    add new middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 
 
@@ -26,6 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
