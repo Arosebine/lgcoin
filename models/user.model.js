477cae4d72a7faaa6adb82505b46fc692ad18b20
arosebine <arebine@gmail.com>
2022-12-22 20:49:34 +0100

    image update, readme

--- models/user.model.js
+++ models/user.model.js
@@ -78,8 +78,8 @@ const userSchema = new mongoose.Schema({
     default: 'user',
   },
   referral: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Referral',
+    type: String,
+    
   },
   status: {
          type: String,
